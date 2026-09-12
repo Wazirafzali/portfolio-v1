039cb40c6d55b55a8759011cc92be3b507cf0c47
Wazir Afzali <wazirafzali57@gmail.com>
2026-09-12 16:59:48 +0430

    Enable Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -82,7 +83,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
